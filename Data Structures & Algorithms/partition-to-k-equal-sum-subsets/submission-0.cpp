ce5a6a7c95a669eb0b4fb957f6372bdf7c5b3cc7
Rashid <142145465+Rashidiiitd@users.noreply.github.com>
2026-08-22 12:23:22 +0530

    Add: partition-to-k-equal-sum-subsets - submission-0

---
+++ Data Structures & Algorithms/partition-to-k-equal-sum-subsets/submission-0.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    bool dfs(vector<int>& nums, vector<bool>& visited, int target, int currSum, int idx, int k) {
+        if (k == 1) return true;
+
+        if (currSum == target) {
+            return dfs(nums, visited, target, 0, 0, k - 1); 
+        }
+
+        for (int i = idx; i < nums.size(); i++) {
+            if (visited[i] || currSum + nums[i] > target) continue;
+
+            visited[i] = true; 
+            
+            if (dfs(nums, visited, target, currSum + nums[i], i + 1, k)) {
+                return true;
+            }
+            
+            visited[i] = false;
+        }
+
+        return false;
+    }
+
+    bool canPartitionKSubsets(vector<int>& nums, int k) {
+        int totalSum = accumulate(nums.begin(), nums.end(), 0);
+        if (totalSum % k != 0) return false;
+
+        int partitionSum = totalSum / k;
+        
+        sort(nums.begin(), nums.end(), greater<int>());
+        if (nums[0] > partitionSum) return false;
+
+        vector<bool> visited(nums.size(), false);
+        return dfs(nums, visited, partitionSum, 0, 0, k);
+    }
+};
